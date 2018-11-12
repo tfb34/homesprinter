b806eb22a6efd73d4a2927e65886b4b7ff9bcfad
Thalia <britothalia212@gmail.com>
2018-11-12 12:38:16 -0500

    Create homepage and about pages

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,4 +4,13 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get home" do
+  	get root_url
+  	assert_response :success
+  end
+
+  test "should get about" do
+  	get welcome_about_url
+  	assert_response :success
+  end
 end
