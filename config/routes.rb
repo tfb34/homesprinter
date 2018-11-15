26a97f733fef65876baf4aa08f7159af2d10aa41
Thalia <britothalia212@gmail.com>
2018-11-15 18:42:46 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'welcome#index'
-  get 'welcome/about'
+  get '/about',  to: 'welcome#about'
+  get '/signup', to: 'users#new'
+  resources :users
 end
